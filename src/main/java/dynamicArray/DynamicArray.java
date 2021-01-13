64b0d2b67dc9c00ba97bbc45f45694aa4fd8211d
yashbhatt <yash.bhatt.1494@gmail.com>
2021-01-13 17:04:06 -0600

    initial commit

---
+++ src/main/java/dynamicArray/DynamicArray.java
@@ -0,0 +1,25 @@
+package dynamicArray;
+
+import java.util.Iterator;
+
+@SuppressWarnings("unchecked")
+public class DynamicArray<T> implements Iterable<T>{
+
+    private T[] arr;
+    private int len = 0;
+    private int capacity = 0;
+
+    public DynamicArray(int capacity)  {
+        //if(capacity < 0) throw new IllegalAccessException("illegal Capacity: ");
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+    
+    public DynamicArray() {
+        this(16);
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
